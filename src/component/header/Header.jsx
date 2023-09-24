625ce004edde7234d648a0d67d7ba8d6ac933d9a
romeoscript <romeobourne211@gmail.com>
2023-09-24 05:12:14 +0100

    feat:manychanges

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import './header.css'
 import logo from '../../assets/react.svg'
-
+import TransitionsModal from './Modal';
+import Skeleton from '@mui/material/Skeleton';
 export default function Header() {
     const [link, setLink] = useState('');
     const [videoDetails, setVideoDetails] = useState(null);
+    const [info, setInfo] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    // const showModal =() =>{
+
+    // }
 
     const extractVideoID = (url) => {
         let videoIDPattern = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
@@ -21,24 +28,28 @@ export default function Header() {
 
         const apiKey = 'AIzaSyDIf9X3nxHznmwhX1aLDx93_vyB4HAlIus';
         const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoID}&key=${apiKey}&part=snippet`;
-
+        setLoading(true)
         fetch(apiUrl)
             .then(response => response.json())
             .then(data => {
                 if (data.items && data.items.length > 0) {
                     setVideoDetails(data.items[0].snippet);
+                  
                 } else {
                     alert('Video not found.');
+                 
                 }
+                setLoading(false)
             })
             .catch(error => {
                 alert('Error fetching video details.');
+                setLoading(false)
             });
     };
 
     return (
         <div className='container'>
-            <div className='menu'>
+            <nav className='menu'>
                 <div className="logo">
                     <h1>YouFame</h1>
                 </div>
@@ -46,31 +57,37 @@ export default function Header() {
                     <p>Home</p>
                     <p>How It Works</p>
                 </div>
-            </div>
+            </nav>
             <div className="menuheader">
                 <h1>Top-Quality YouTube Engagement Services</h1>
                 <p>Our offerings go beyond mere views. We provide YouTube subscribers, likes, genuine <br /> comments, and shares to enhance your channel's performance.</p>
-                <div>
-                    <h2>Order Here</h2>
-                    <img src="icons/arrow.png" alt="" />
-                    <input type="text" value={link}
-
+                <div className='input_container'>
+                    <input type="search" value={link}
                         onChange={(e) => {
                             setLink(e.target.value);
-                            fetchVideoDetails();
                         }}
                         placeholder='Search video or paste link (youtube.com/watch? =xyz' />
                     <button className='start' onClick={fetchVideoDetails}>START</button>
-                    {videoDetails && <aside>
-                        <img src={videoDetails.thumbnails.default.url } alt="" />
-                        <div>
-                            <h2>{videoDetails.title }</h2>
-                            <p>{(videoDetails.description.length > 100 ? videoDetails.description.substring(0, 100) + "..." : videoDetails.description)}</p>
-                        </div>
-                    </aside>
-                    }
-
+                    {info && <TransitionsModal setInfo={setInfo} video={videoDetails} />}
+                    {loading ? (
+                        <aside>
+                            <Skeleton variant="rounded" width={210} height={60} />
+                            <div>
+                                <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                                <Skeleton variant="rounded" width={'100%'} height={40} />
+                            </div>
+                        </aside>
+                    ) : videoDetails && (
+                        <aside onClick={() => setInfo(true)}>
+                            <img src={videoDetails.thumbnails.high.url} alt="" />
+                            <div>
+                                <h2>{videoDetails.title.length > 70 ? videoDetails.title.substring(0, 70) + "..." : videoDetails.title}</h2>
+                                <p>{(videoDetails.description.length > 100 ? videoDetails.description.substring(0, 100) + "..." : videoDetails.description)}</p>
+                            </div>
+                        </aside>
+                    )}
                 </div>
+
             </div>
         </div>
     )
---
+++ src/component/header/Modal.jsx
@@ -0,0 +1,173 @@
+import { useEffect, useState } from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Slider, { SliderThumb } from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import PaidIcon from '@mui/icons-material/Paid';
+import Stack from '@mui/material/Stack';
+import './header.css'
+import service from '../../../public/icons/services.png'
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+    borderRadius: '10px',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+
+
+const PrettoSlider = styled(Slider)({
+    color: '#52af77',
+    height: 8,
+    '& .MuiSlider-track': {
+        border: 'none',
+    },
+    '& .MuiSlider-thumb': {
+        height: 24,
+        width: 24,
+        backgroundColor: '#fff',
+        border: '2px solid currentColor',
+        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+            boxShadow: 'inherit',
+        },
+        '&:before': {
+            display: 'none',
+        },
+    },
+    '& .MuiSlider-valueLabel': {
+        lineHeight: 1.2,
+        fontSize: 12,
+        background: 'unset',
+        padding: 0,
+        width: 32,
+        height: 32,
+        borderRadius: '50% 50% 50% 0',
+        backgroundColor: '#52af77',
+        transformOrigin: 'bottom left',
+        transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+        '&:before': { display: 'none' },
+        '&.MuiSlider-valueLabelOpen': {
+            transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+        },
+        '& > *': {
+            transform: 'rotate(45deg)',
+        },
+    },
+});
+export default function TransitionsModal({ setInfo, video }) {
+    const [open, setOpen] = useState(true);
+    const [data, setData] = useState({
+        viewsSlider: 500,
+        likesSlider: 80,
+        subscribersSlider: 20,
+    });
+    const handleSliderChange = (type, newValue) => {
+        setData(prevData => ({
+            ...prevData,
+            [type]: newValue,
+            [type.replace("Slider", "")]: newValue
+        }))
+
+    }
+    const handleClose = () => {
+        setInfo(false)
+        setOpen(false);
+       
+    }
+
+    console.log(video.thumbnails.default.url);
+    return (
+        <div className='modal_figure' >
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <div className='image_father'>
+                            <p>{video?.title}</p>
+                            <span>by {video?.channelTitle}</span>
+                            <img src={video?.thumbnails.high.url} alt="" className='image' />
+                        </div>
+                        <Box sx={{ width: '100%' }}>
+
+                            <p className='view_Info'>
+                                <span>{data.viewsSlider} views</span>
+                                <span>${data.viewsSlider * 1.3}</span>
+                            </p>
+                            <PrettoSlider
+                                valueLabelDisplay="auto"
+                                aria-label="pretto slider"
+                                value={data.viewsSlider}
+                                step={1000}
+                                marks
+                                onChange={(e, val) => handleSliderChange('viewsSlider', val)}
+                                max={10000}
+                            />
+                            <p className='view_Info'>
+                                <span>{data.likesSlider} Likes</span>
+                                <span>${data.likesSlider * 2}</span>
+                            </p>
+                            <PrettoSlider
+                                valueLabelDisplay="auto"
+                                aria-label="pretto slider"
+                                value={data.likesSlider}
+                                step={10}
+                                marks
+                                onChange={(e, val) => handleSliderChange('likesSlider', val)}
+                                max={1000}
+                            />
+                            <p className='view_Info'>
+                                <span>{data.subscribersSlider}Subscribers</span>
+                                <span>${data.subscribersSlider * 0.4}</span>
+                            </p>
+                            <PrettoSlider
+                                valueLabelDisplay="auto"
+                                aria-label="pretto slider"
+                                value={data.subscribersSlider}
+                                marks
+                                onChange={(e, val) => handleSliderChange('subscribersSlider', val)}
+                                max={500}
+                            />
+
+                        </Box>
+                        <h2 className='modal_details'>You will receive <span>{data.viewsSlider}</span> views, <span>{data.likesSlider}</span> likes, and <span>{data.subscribersSlider}</span> subscribers</h2>
+                        <figure>
+                            <h3>TOTAL= $15</h3>
+                            <Stack direction="row" spacing={2}>
+                                <Button variant="outlined" startIcon={<CreditCardIcon />}>
+                                    Credit Card
+                                </Button>
+                                <Button variant="contained" startIcon={<PaidIcon />}>
+                                    Cryptocurrency
+                                </Button>
+                            </Stack>
+                        </figure>
+                    </Box>
+
+                </Fade>
+            </Modal>
+        </div>
+    );
+}
