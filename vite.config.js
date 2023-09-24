cf8714151a87b60290b7ff0168d21cb2e02c3402
romeoscript <romeobourne211@gmail.com>
2023-09-24 23:40:31 +0100

    feat:fixing

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@mui/material/Skeleton']
+      external: ['@mui/material/Skeleton', '@mui/material/Backdrop']
     }
   }
 });
